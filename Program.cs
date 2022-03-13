89e0fbd12890907246e5ccd4598320b0fba89202
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-03-13 08:45:00 -0300

    Testando C#

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace first_project_angular
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        
+            Console.Write("Write your last name: ");String lastName = Console.ReadLine();
+            Console.Write("Write your first name: ");String firstName = Console.ReadLine();
+            Console.Write("Write your years old: ");var idade = Console.ReadLine();
+            //String lastName = Console.ReadLine();
+            Console.WriteLine("Your name is "+lastName+" "+firstName+", and your years old is "+idade+"\nThank you very much! ");
+        
         }
     }
 }
